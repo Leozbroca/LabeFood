f6e00dbd14820d32d855f4724e4282c94107eac9
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-11 14:16:47 -0300

    login e cadastro

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsB'
+ const BASE_URL = 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsB'
+export default BASE_URL
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,54 @@
 import React from "react";
+import axios from 'axios'
+import useForm from "../../hooks/useForm";
+import { StyledButton, StyledInput } from './styles'
+import BASE_URL from "../../constants/url";
+import { useNavigate } from "react-router";
+import { goToHome, goToSignUp } from "../../router/coordinator";
+
 const LoginPage = () => {
+    const { form, onChangeInput, clear } = useForm({ email: '', password: '' })
+    const navigate = useNavigate()
+  
+    const login = (event) => {
+        event.preventDefault()
+        axios
+            .post(`${BASE_URL}/login`, form)
+            .then((res)=>{
+                localStorage.setItem('token', res.data.token)
+                clear()
+                goToHome(navigate)
+            })
+            .catch((err)=>{
+                console.log('loginer', err)
+            })
+    }
+    // onClick={goToSignUp(navigate)}
     return (
         <div>
             <h1>LoginPage</h1>
+            <form onSubmit={login}>
+                <StyledInput
+                    name='email'
+                    value={form.email}
+                    onChange={onChangeInput}
+                    id="outlined-basic"
+                    label="E-mail"
+                    variant="outlined" />
+
+                <StyledInput
+                    name='password'
+                    value={form.password}
+                    onChange={onChangeInput}
+                    id="outlined-basic"
+                    label="Senha"
+                    variant="outlined" />
+
+                <StyledButton color='secondary' variant="contained" type="submit">Login</StyledButton>
+                <div onClick={()=> goToSignUp(navigate)}>
+                    Não possui cadastro? Clique aqui. 
+                </div>
+            </form>
         </div>
     )
 }
